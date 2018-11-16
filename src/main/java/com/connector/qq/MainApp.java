49b98a7bed8ee16aaa86f9008c331a47d111cbfc
Robert Stine <RobWStine@outlook.com>
2018-11-16 16:40:47 -0500

    Changed to build to a war to deploy to tomcat.
    Added configuration for writing to disk or not.
    
    Added configuration on which folder to write to.
    
    Added error handling for failing to write file to disk.
    
    Added field for AWS key if AWS API Key is asked for.

--- src/main/java/com/connector/qq/MainApp.java
+++ src/main/java/com/connector/qq/MainApp.java
@@ -1,23 +1,20 @@
 package com.connector.qq;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jms.JmsException;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@ComponentScan("com.connector.qq")
-public class MainApp {
+public class MainApp extends SpringBootServletInitializer  {
 
-    @Autowired
-    private JmsTemplate queueTemplate;
+    /*
+    * Uncomment main method and delete SpringBootServletInitializer to package as .jar / stand alone micro-service.
+    * */
 
+   /*
     public static void main(String[] args) {
+
         SpringApplication.run(MainApp.class, args);
     }
-
+    */
 }
